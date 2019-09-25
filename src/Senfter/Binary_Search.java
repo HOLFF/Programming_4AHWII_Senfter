74dad0645e67d5f8c4e5fe5d438817f1100413ad
HOLFF <leerharter@gmail.com>
2019-09-25 10:21:17 +0200

    formatted code

--- src/Senfter/Binary_Search.java
+++ src/Senfter/Binary_Search.java
@@ -3,40 +3,42 @@ package Senfter;
 import java.util.Arrays;
 
 public class Binary_Search {
-    public static void main(String[] args){
-    int[] rans = befüllen(100);
-    pos(rans,50);
-   //ausgabe(rans);
-
-
-
+    public static void main(String[] args) {
+        int[] rans = befüllen(100);
+        pos(rans, 50);
+        //ausgabe(rans);
     }
-   public static void pos(int[]arr,int toS){
-        int pos=arr.length/2;
-        int schrittweite=pos/2;
-        for(int i =0;i< (int)(Math.log(arr.length)/Math.log(2))+1;i++) {
-            if(arr[pos]!=toS) {
+
+    public static void pos(int[] arr, int toS) {
+        int pos = arr.length / 2;
+        int schrittweite = pos / 2;
+        for (int i = 0; i < (int) (Math.log(arr.length) / Math.log(2)) + 1; i++) {
+            if (arr[pos] != toS) {
                 if (arr[pos] > toS) pos -= schrittweite;
                 else if (arr[pos] < toS) pos += schrittweite;
                 schrittweite = schrittweite / 2;
                 if (schrittweite == 0) schrittweite = 1;
             }
         }
-        if(arr[pos]==toS){System.out.println("Zahl auf Position: "+pos);System.out.println("Gesuchte Zahl: "+toS+"   Zahl auf gefundener Position: "+arr[pos]);}
-        else System.out.println("Zahl nicht vorhanden");
+        if (arr[pos] == toS) {
+            System.out.println("Zahl auf Position: " + pos);
+            System.out.println("Gesuchte Zahl: " + toS + "   Zahl auf gefundener Position: " + arr[pos]);
+        } else System.out.println("Zahl nicht vorhanden");
     }
-    public static int[] befüllen(int laenge){
-        int[] nums= new int[laenge];
-        for(int i=0;i<laenge;i++){
-            int random = (int )(Math.random() * laenge + 1);
-            nums[i]= random;
+
+    public static int[] befüllen(int laenge) {
+        int[] nums = new int[laenge];
+        for (int i = 0; i < laenge; i++) {
+            int random = (int) (Math.random() * laenge + 1);
+            nums[i] = random;
         }
         Arrays.sort(nums);
         return nums;
     }
-    public static void ausgabe(int[] arr){
-        for(int i = 0;i<arr.length;i++){
-            System.out.println(i+": "+arr[i]);
+
+    public static void ausgabe(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i + ": " + arr[i]);
         }
     }
 }
